31a932ce477dfb7fbd614b9ff1a18be24e60ef46
AmbarR0 <ambarrojasm@gmail.com>
2023-09-15 07:48:55 -0400

    final final commit

--- scripts/last_scene.gd
+++ scripts/last_scene.gd
@@ -15,3 +15,7 @@ func _on_leave_button_down():
 
 func _on_control_send_end(time):
 	score.text = str(time)
+
+
+func _on_retry_button_down():
+	get_tree().change_scene_to_file("res://scenes/control.tscn")
